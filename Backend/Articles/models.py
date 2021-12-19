3f85093414c09e0cb30b8e22ab075e4afa8a3d05
forest <formcsp@gmail.com>
2021-12-19 14:34:13 +0300

    Created applications Articles, News and Reviews. Added models for Articles and registered on admin panel

---
+++ Backend/Articles/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    """
+    Model for articles category objects
+    """
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория статей'
+        verbose_name_plural = 'Категории статей'
+
+
+class HashTags(models.Model):
+    """
+    Model for article hashtag objects
+    """
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Хэштег'
+        verbose_name_plural = 'Хэштеги'
+
+
+class Article(models.Model):
+    """
+    Model for objects of articles
+    """
+    title = models.CharField(max_length=255)
+    text = models.TextField(max_length=4096)
+    publication_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='Media/images/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    hash_tags = models.ManyToManyField(HashTags)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
